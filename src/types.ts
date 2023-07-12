8fa9f319e18947e1c3d36c9d963828576cd0ddbe
Justin Kuhn <justintkuhn@gmail.com>
2023-07-12 18:26:36 +0100

    types

---
+++ src/types.ts
@@ -0,0 +1,49 @@
+export type MAP_COLUMNS =  "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" | "K";
+export type MAP_ROWS = 1 | 2 | 3 | 4 | 5;
+export type BuildingType = "Colony Hub" | "Rock Mine" | "Ice Mine" | "Factory" | "Biome/Farm" | 
+    "Temple" | "Barracks" | "Theatre" | "School" | "University" | "Hospital" | "Science Lab" | 
+    "Launch Pad" | "Superconductor";
+
+export type Faction = {
+    id: number;
+    name: string;
+    resourcesOwned: Resource[];
+    tilesOwned: Tile[];
+    provides: Colonist[];
+    wants: Resource[];
+}
+
+export type Resource = {
+    id: number;
+    name: string;
+    quantity: number;
+}
+
+export type Tile = {
+    column: MAP_COLUMNS;
+    row: MAP_ROWS;
+    resources: Resource[];
+    colonizedBy: Faction;
+}
+
+export type Building = {
+    type: BuildingType;
+    cost: Resource[];
+    produce: Resource[];
+}
+
+export type Colonist = {
+    type: string;
+    subtype: string;
+    occupies: Building;
+    factionSupply: Faction;
+    cost: Resource[];
+}
+
+export type Leader = {
+    type: string;
+    building: Building;
+}
+
+
+
