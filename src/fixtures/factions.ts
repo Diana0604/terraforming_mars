0cfe6940908b9d17ea454da7123e43809cf50052
Diana0604 <diana.vallverdu@gmail.com>
2023-07-15 21:22:49 +0100

    faction fixtures

---
+++ src/fixtures/factions.ts
@@ -0,0 +1,36 @@
+import { Faction } from "@/types";
+
+const factionFixtures: Faction[] = [
+  {
+    id: 0,
+    name: "The Federation",
+    resourcesOwned: [],
+    tilesOwned: [],
+    provides: [],
+    wants: [],
+  },
+  {
+    id: 1,
+    name: "The Human Coalition",
+    resourcesOwned: [],
+    tilesOwned: [],
+    provides: [],
+    wants: [],
+  },
+  {
+    id: 2,
+    name: "APETA (Astro-Physics and EngineeringTech Agency)",
+    resourcesOwned: [],
+    tilesOwned: [],
+    provides: [],
+    wants: [],
+  },
+  {
+    id: 3,
+    name: "The Gardeners",
+    resourcesOwned: [],
+    tilesOwned: [],
+    provides: [],
+    wants: [],
+  },
+];
