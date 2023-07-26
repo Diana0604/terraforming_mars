8c612419e5bd5eb11b709b8938a950852b868dc9
Diana0604 <diana.vallverdu@gmail.com>
2023-07-26 10:25:28 +0100

    getters for corporation and tile from database

---
+++ src/app/api/corporation/route.tsx
@@ -0,0 +1,22 @@
+import corporationModel from '@/functions/database/models/corporation.model'
+import { NextResponse } from 'next/server'
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url)
+  const id = searchParams.get('id')
+  const name = searchParams.get('name')
+
+  if (!id && !name) return NextResponse.json({ error: "At least property id or name is needed in params" }, { status: 400 })
+
+  let corporation;
+
+  if (id) {
+    corporation = await corporationModel.findById(id)
+  } else {
+    corporation = await corporationModel.findOne({ name: name })
+  }
+  if (!corporation) return NextResponse.json({ error: "Corporation not found" }, { status: 500 })
+
+
+  return NextResponse.json({ corporation })
+}
