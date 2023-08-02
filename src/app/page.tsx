e142e0b585b95ba005434e8f1ee67ff10c352b2f
Justin Kuhn <justintkuhn@gmail.com>
2023-08-02 10:08:37 +0100

    tooltip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 "use client"
-import Image from 'next/image'
 import styles from './page.module.css'
 import Chart from './map/chart';
-import map from "./bareMap.png";
-import { useEffect } from 'react';
 import Timer from './map/timer';
 import InfoBar from './map/infoBar';
 
