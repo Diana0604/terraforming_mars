553654c43072779ddf342ee558aa81af7d3f6a97
Diana0604 <diana.vallverdu@gmail.com>
2023-08-14 13:32:15 +0100

    mongoUri depends on deployment status

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
+const env =
+  process.env.NODE_ENV === "development"
+    ? {
+        mongoUri: "mongodb://127.0.0.1:27017/terraforming-mars",
+      }
+    : { mongoUri: process.env.MONGO_URI };
+
 module.exports = {
-  env: {
-    mongoUri: "mongodb://127.0.0.1:27017/terraforming-mars",
-  },
+  env: env,
 };
