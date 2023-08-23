a1f6b533762d47f303ccc196ed5b97b09bdcff6e
Diana0604 <diana.vallverdu@gmail.com>
2023-08-23 12:32:44 +0100

    add round manager context use of actors page

--- src/app/actors/round/RoundManager.tsx
+++ src/app/actors/round/RoundManager.tsx
@@ -1,47 +1,14 @@
 "use client"
 import { PAUSE_GAME, PLAY_GAME, ROUND_MANAGER_ROUTE } from "@/constants"
-import { Round } from "@/types"
+import { RoundContext } from "@/contexts/RoundContext"
 import { Button, Card } from "antd"
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 
 const TurnManager = () => {
 
-  const [round, setRound] = useState<Round>()
+  const { round } = useContext(RoundContext)
 
   const [buttonsDisabled, setButtonsDisabled] = useState<boolean>(false)
-  const [timeInterval, setTimeInterval] = useState<NodeJS.Timer>()
-
-  useEffect(() => {
-
-    //get round and set state
-    fetch(ROUND_MANAGER_ROUTE, { method: "get" }).then(async (response: Response) => {
-      const data = await response.json();
-      if (data.error) return //TO DO: set display message
-      if (data.startTime) data.startTime = new Date(data.startTime)
-      setRound(data)
-    })
-
-  }, [])
-
-  //check round time
-  useEffect(() => {
-    if (!round) return;
-
-    if (!round.playing) return clearInterval(timeInterval)
-
-    const timeInt = setInterval(async () => {
-      const res = await fetch(ROUND_MANAGER_ROUTE, { method: "get" })
-      const data = await res.json()
-      if (!data.playing) {
-        if (data.startTime) data.startTime = new Date(data.startTime)
-        setRound(data)
-      }
-    }, 5000)
-    setTimeInterval(timeInt)
-
-    return () => clearInterval(timeInt)
-  }, [round])
-
 
   const onClickPlay = async () => {
     setButtonsDisabled(true)
@@ -49,7 +16,6 @@ const TurnManager = () => {
     const data = await res.json();
     if (data.error) return; //TODO: set display message
     if (data.startTime) data.startTime = new Date(data.startTime)
-    setRound(data)
     setButtonsDisabled(false)
   }
 
@@ -60,7 +26,6 @@ const TurnManager = () => {
     const data = await res.json();
     if (data.error) return; //TODO: set display message
     if (data.startTime) data.startTime = new Date(data.startTime)
-    setRound(data)
     setButtonsDisabled(false)
   }
 
@@ -70,10 +35,13 @@ const TurnManager = () => {
 
       {round && (
         <div>
+
           <div>CurrentRound: {round.number} </div>
-          <div> Round Started At: {(round && round.startTime) ? `${round.startTime.getHours()}:${round.startTime.getMinutes()}:${round.startTime.getSeconds()}` : 'round not started'} </div>
           <div>Status: {round.playing ? "Playing" : "Paused"}</div>
           <div>Dark Hour: {round.darkHour ? "Active" : "Inactive"}</div>
+          {
+            (round.startTime) && <div>Round Started At: {`${round.startTime.getHours()}:${round.startTime.getMinutes()}:${round.startTime.getSeconds()}`} </div>
+          }
         </div>
       )}
       <Button disabled={buttonsDisabled || !round || (round.playing)} onClick={onClickPlay}>Play</Button>
