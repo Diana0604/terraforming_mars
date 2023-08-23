bb3a3b7eefc161210d4758b4f516c534e339d8c8
Diana0604 <diana.vallverdu@gmail.com>
2023-08-23 13:13:12 +0100

    separate corporation stats to different fixtures

---
+++ src/fixtures/corporationsResources.ts
@@ -0,0 +1,24 @@
+import {
+  FOOD_NAME,
+  MINERALS_NAME,
+  OXYGEN_NAME,
+  RARE_METAL_NAME,
+  WATER_NAME,
+} from "@/constants";
+
+export default {
+  playerCorporationResources: [
+    { name: WATER_NAME, quantity: 100 },
+    { name: RARE_METAL_NAME, quantity: 100 },
+    { name: OXYGEN_NAME, quantity: 100 },
+    { name: FOOD_NAME, quantity: 100 },
+    { name: MINERALS_NAME, quantity: 100 },
+  ],
+  actorsCorporationResources: [
+    { name: WATER_NAME, quantity: 100 },
+    { name: RARE_METAL_NAME, quantity: 100 },
+    { name: OXYGEN_NAME, quantity: 100 },
+    { name: FOOD_NAME, quantity: 100 },
+    { name: MINERALS_NAME, quantity: 100 },
+  ],
+};
