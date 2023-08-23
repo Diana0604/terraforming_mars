794b1f268ed1f7900b07899f2878352e7e1e752a
Diana0604 <diana.vallverdu@gmail.com>
2023-08-23 12:31:57 +0100

    add round context

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,15 @@
 "use client"
 import { CorporationsContextProvider } from "@/contexts/CorporationsContexts"
 import { TilesContextProvider } from "@/contexts/TileContext"
+import { RoundContextProvider } from "@/contexts/RoundContext"
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return <div>
     <CorporationsContextProvider>
       <TilesContextProvider>
-        {children}
+        <RoundContextProvider>
+          {children}
+        </RoundContextProvider>
       </TilesContextProvider>
     </CorporationsContextProvider>
   </div>
---
+++ src/contexts/RoundContext.tsx
@@ -0,0 +1,62 @@
+
+import { ROUND_MANAGER_ROUTE } from "@/constants";
+import { Round } from "@/types";
+import React, { useState, useEffect } from "react";
+
+//tiles can be accessed from context
+type contextProps = {
+  round: Round
+}
+
+//initial corporation values
+const initialRoundProps: contextProps = {
+  round: {
+    number: 0,
+    playing: false,
+    darkHour: false
+  }
+};
+
+//build context
+export const RoundContext: React.Context<contextProps> = React.createContext(initialRoundProps);
+
+//build context provider
+export const RoundContextProvider = ({ children }: { children: React.ReactNode }) => {
+
+  //corporation objects and setters
+  const [round, setRound] = useState<Round>(initialRoundProps.round)
+
+
+  useEffect(() => {
+
+    //update round info every second by fetching from database
+    const updateRound = () => {
+      try {
+        fetch(ROUND_MANAGER_ROUTE, { method: 'get' }).then(async response => {
+          const data = await response.json();
+          data.startTime = new Date(data.startTime)
+          setRound(data)
+        }, (error) => { console.log('error on fetch', error) })
+      } catch (error) {
+        console.log('error on fetch', error)
+      }
+    }
+
+    updateRound()
+
+    const interval = setInterval(updateRound, 1000)
+
+    //destroy interval at dismount
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
+
+  //return provider values
+  return (
+    <RoundContext.Provider value={{ round }}>
+      {children}
+    </RoundContext.Provider>
+  );
+};
+
