45b182ed90fb610676c8a46954f332f4cbb17f17
Diana0604 <diana.vallverdu@gmail.com>
2023-08-24 18:06:59 +0100

    undo image changes from infobar as it messes up styles in deployment

--- src/app/map/infoBar.tsx
+++ src/app/map/infoBar.tsx
@@ -3,48 +3,47 @@ import { useContext, useEffect, useState } from "react";
 import styles from "../page.module.css"
 import { Resource } from "@/types";
 import { CorporationsContext } from "@/contexts/CorporationsContexts";
-import Image from "next/image"
 
 const InfoBar = () => {
 
     const [resources, setResources] = useState<Resource[]>();
-    const { playerCorporation } = useContext(CorporationsContext)
+    const {playerCorporation} = useContext(CorporationsContext)
 
     useEffect(() => {
         setResources(playerCorporation.resourcesOwned)
     }, [playerCorporation])
 
-    const switchIcons = (name: string) => {
-        switch (name) {
+    const switchIcons = (name:string) => {
+        switch(name) {
             case "Rare Metal":
-                return <div className={styles.icons}><Image fill={true} alt='icon for gold material' src="/icons/gold.png" />;</div>
+                return <img className={styles.icons} src="/icons/gold.png"/>;
             case "Water":
-                return <div className={styles.icon}><Image fill={true} alt='icon for water material' src="/icons/waterdrop.png" /></div>;
+                return <img className={styles.icons} src="/icons/waterdrop.png"/>;
             case "Oxygen":
-                return <div className={styles.icon}><Image fill={true} alt='icon for oxygen material' src="/icons/oxygen-tank.png" /></div>;
+                return <img className={styles.icons} src="/icons/oxygen-tank.png"/>;
             case "Food":
-                return <div className={styles.icon}><Image fill={true} alt='icon for food material' src="/icons/fork.png" /></div>;
+                return <img className={styles.icons} src="/icons/fork.png"/>;
             case "Synthetics":
-                return <div className={styles.icon}><Image fill={true} alt='icon for synthetics material' src="/icons/plastic-bottle.png" /></div>;
+                return <img className={styles.icons} src="/icons/plastic-bottle.png"/>;
             case "Minerals":
-                return <div className={styles.icon}><Image fill={true} alt='icon for minerals material' src="/icons/rock.png" /></div>;
+                return <img className={styles.icons} src="/icons/rock.png"/>;
             default:
-                return <div className={styles.icon}><Image fill={true} alt='icon for generic material' src="/icons/fork.png" /></div>;
-
+                return <img className={styles.icons} src="/icons/fork.png"/>;
+        
         }
     }
 
     return (
         <div className={styles.infoBar}>
             <p>{playerCorporation.name}</p>
-            {resources && resources.map((resource, index) => {
-                return <div key={index} style={{ padding: "5px" }}>
-                    <div style={{ width: "30px" }}>
-                        {switchIcons(resource.name)}
+            { resources && resources.map((resource, index) => {
+                return <div key={index} style={{padding: "5px"}}>
+                   <div style={{width: "30px"}}>
+                    {switchIcons(resource.name)}
+                   </div>
+                   {/* <p>{resource.name}</p>  */}
+                   <p>{resource.quantity}</p> 
                     </div>
-                    {/* <p>{resource.name}</p>  */}
-                    <p>{resource.quantity}</p>
-                </div>
             })}
         </div>
     )
