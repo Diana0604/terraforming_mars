2f43d4e9a62c354342db05529281c44b7885c735
Diana0604 <diana.vallverdu@gmail.com>
2023-08-24 18:26:24 +0100

    revert changes in timer

--- src/app/map/timer.tsx
+++ src/app/map/timer.tsx
@@ -9,32 +9,31 @@ const Timer = () => {
 
     const [hydrated, setHydrated] = useState(false);
     //time in milliseconds
-    const gameClockInterval = 1000 * SECONDS_PER_ROUND/ (1440/5) ;
     const [displayTime, setDisplayTime] = useState<Date>(new Date(Date.now()))
     const [round, setRound] = useState<Round>();
     const roundContext = useContext(RoundContext);
+
+    const resetClock = () => {
+
+        setDisplayTime(time => {
+          if (roundContext.round.startTime) {
+          let newTime = new Date()
+          newTime.setHours(0,0,0,0)
+          let startTime:Date = roundContext.round.startTime
+          let elapsedTime = (Date.now() - startTime.getTime())
+          let elapsedGameTime = elapsedTime*(86400/SECONDS_PER_ROUND)
+          time.setTime(newTime.getTime() + elapsedGameTime);
+
+          //floor to nearest 5 minutes
+          time.setMinutes(time.getMinutes() - time.getMinutes()%5)
+
+          }
+          return time
+        })
+
+      
+    }
     useEffect(() => {
-
-        const resetClock = () => {
-
-          setDisplayTime(time => {
-            if (roundContext.round.startTime) {
-            let newTime = new Date()
-            newTime.setHours(0,0,0,0)
-            let startTime:Date = roundContext.round.startTime
-            let elapsedTime = (Date.now() - startTime.getTime())
-            let elapsedGameTime = elapsedTime*(86400/SECONDS_PER_ROUND)
-            time.setTime(newTime.getTime() + elapsedGameTime);
-
-            //floor to nearest 5 minutes
-            time.setMinutes(time.getMinutes() - time.getMinutes()%5)
-
-            }
-            return time
-          })
-
-        
-      }
       setHydrated(true);
       //initial display of time
       resetClock()
@@ -48,6 +47,11 @@ const Timer = () => {
       // Returns null on first render, so the client and server match
       return null;
     }
+
+
+
+
+
 
     return (
       <div>
