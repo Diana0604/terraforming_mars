9d9e5afbacd27e654f3d02550c8e1b6c015edcce
Diana0604 <diana.vallverdu@gmail.com>
2023-10-01 13:53:10 +0100

    info bar with resources next round set up

--- src/app/map/infoBar.tsx
+++ src/app/map/infoBar.tsx
@@ -3,48 +3,58 @@ import { useContext, useEffect, useState } from "react";
 import styles from "../page.module.css"
 import { Resource } from "@/types";
 import { CorporationsContext } from "@/contexts/CorporationsContexts";
+import { Col, Row } from 'antd';
 
 const InfoBar = () => {
 
     const [resources, setResources] = useState<Resource[]>();
-    const {playerCorporation} = useContext(CorporationsContext)
+    const [resourcesNextRound, setResourcesNextRound] = useState<Resource[]>();
+    const { playerCorporation } = useContext(CorporationsContext)
 
     useEffect(() => {
         setResources(playerCorporation.resourcesOwned)
+
+        setResourcesNextRound(playerCorporation.resourcesNextRound)
     }, [playerCorporation])
 
-    const switchIcons = (name:string) => {
-        switch(name) {
+    const switchIcons = (name: string) => {
+        switch (name) {
             case "Rare Metal":
-                return <img className={styles.icons} src="/icons/gold.png"/>;
+                return <img className={styles.icons} src="/icons/gold.png" />;
             case "Water":
-                return <img className={styles.icons} src="/icons/waterdrop.png"/>;
+                return <img className={styles.icons} src="/icons/waterdrop.png" />;
             case "Oxygen":
-                return <img className={styles.icons} src="/icons/oxygen-tank.png"/>;
+                return <img className={styles.icons} src="/icons/oxygen-tank.png" />;
             case "Food":
-                return <img className={styles.icons} src="/icons/fork.png"/>;
+                return <img className={styles.icons} src="/icons/fork.png" />;
             case "Synthetics":
-                return <img className={styles.icons} src="/icons/plastic-bottle.png"/>;
+                return <img className={styles.icons} src="/icons/plastic-bottle.png" />;
             case "Minerals":
-                return <img className={styles.icons} src="/icons/rock.png"/>;
+                return <img className={styles.icons} src="/icons/rock.png" />;
             default:
-                return <img className={styles.icons} src="/icons/fork.png"/>;
-        
+                return <img className={styles.icons} src="/icons/fork.png" />;
+
         }
     }
 
     return (
         <div className={styles.infoBar}>
             <p>{playerCorporation.name}</p>
-            { resources && resources.map((resource, index) => {
-                return <div key={index} style={{padding: "5px"}}>
-                   <div style={{width: "30px"}}>
-                    {switchIcons(resource.name)}
-                   </div>
-                   {/* <p>{resource.name}</p>  */}
-                   <p>{resource.quantity}</p> 
-                    </div>
+            {resources && resources.map((resource, index) => {
+                return <Row key={index} style={{ padding: "10px" }}>
+                    <Col span={12} style={{ width: "30px" }} >
+                            <div style={{textAlign: 'center'}}>
+                                {switchIcons(resource.name)}
+                            </div>
+                            <div style={{textAlign: 'center'}}>{resource.quantity} </div>
+                    </Col>
+
+                    <Col span={12}>
+                        {resourcesNextRound && resourcesNextRound.length >= index && <div>{Math.sign(resourcesNextRound[index].quantity - resource.quantity) === 1 && '+'}{resourcesNextRound[index].quantity - resource.quantity}</div>}
+                    </Col>
+                </Row>
             })}
+
         </div>
     )
 }
