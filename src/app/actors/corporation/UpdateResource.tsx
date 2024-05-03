f35c74d516af217f26aa0367deb5bccefe30c182
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2024-05-03 09:13:42 +0100

    refactor add resources methods

--- src/app/actors/corporation/UpdateResource.tsx
+++ src/app/actors/corporation/UpdateResource.tsx
@@ -1,42 +1,61 @@
-"use client"
+"use client";
 //database routes
-import { RESOURCE_DATABASE_ROUTE } from "@/constants"
+import { RESOURCE_DATABASE_ROUTE } from "@/constants";
 
 //react hooks
-import {useState } from "react"
+import { useState } from "react";
 
 interface UpdateStatProp {
-  corporation: string,
-  resource: string,
-  quantity: number
+  corporation: string;
+  resource: string;
+  quantity: number;
 }
 
 /**
  * Update resource input and button to send info to database
  */
 const UpdateResource = (props: UpdateStatProp) => {
-
-  const [displayMessage, setDisplayMessage] = useState<string>()
-  const [quantity, setQuantity] = useState<number>(0)
+  const [displayMessage, setDisplayMessage] = useState<string>();
+  const [quantity, setQuantity] = useState<string>();
 
   const onClick = async () => {
-    setDisplayMessage("Adding resource to database")
-    const res = await fetch(RESOURCE_DATABASE_ROUTE, { method: "post", body: JSON.stringify({ quantity: quantity, corporation: props.corporation, resource: props.resource }) })
+    setDisplayMessage("Adding resource to database");
+
+    //check quantity is a number
+    if (Number.isNaN(Number(quantity))) return alert("Quantity must be number");
+
+    const res = await fetch(RESOURCE_DATABASE_ROUTE, {
+      method: "post",
+      body: JSON.stringify({
+        quantity: quantity,
+        corporation: props.corporation,
+        resource: props.resource,
+      }),
+    });
     const data = await res.json();
     if (data.error) {
-      setDisplayMessage(data.error)
+      setDisplayMessage(data.error);
     }
+    setQuantity("");
     setTimeout(() => {
-      setQuantity(0)
-      setDisplayMessage(undefined)
-    }, 3000)
-  }
+      setDisplayMessage(undefined);
+    }, 3000);
+  };
 
-  return (<div>
-    Add / Remove {props.resource}: <input type="number" value={quantity || 0} onChange={(event) => { setQuantity(Number(event.target.value)) }}></input>
-    <button onClick={onClick}>Add</button>
-    {displayMessage ? displayMessage : <></>}
-  </div>)
-}
+  return (
+    <div>
+      Add / Remove {props.resource}:{" "}
+      <input
+        type="number"
+        value={quantity || ""}
+        onChange={(event) => {
+          setQuantity(event.target.value);
+        }}
+      ></input>
+      <button onClick={onClick}>Add</button>
+      {displayMessage ? displayMessage : <></>}
+    </div>
+  );
+};
 
-export default UpdateResource
+export default UpdateResource;
