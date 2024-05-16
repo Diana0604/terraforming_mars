d3f8efa29cb0f8b24121abcde4ba55aa625558e7
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2024-05-16 13:05:54 +0100

    all seders done but can simplify

--- tests/database.test.ts
+++ tests/database.test.ts
@@ -1,8 +1,12 @@
-import { createAllCorporations, createAllTiles, deleteAllModels } from "../src/functions/database/database.seeder";
+import { createAllCorporations, createAllTiles, deleteAllModels, seedDB } from "../src/functions/database/database.seeder";
 import { closeDatabase, dbConnect } from "../src/functions/database/database.server";
 import tileFixtures from "../src/fixtures/tiles";
 import corporationFixtures from "../src/fixtures/corporation";
+import firstRound from "../src/fixtures/round";
 import { compareWithDatabase } from "./helpers";
+import tileModel from "../src/functions/database/models/tile.model";
+import roundModel from "../src/functions/database/models/round.model";
+import corporationModel from "../src/functions/database/models/corporation.model";
 
 process.env.mongoUri = "mongodb://127.0.0.1:27017/terraforming-mars";
 
@@ -32,6 +36,41 @@ describe('database tests', () => {
 
       for (const index in corporations) {
         expect(compareWithDatabase(corporationFixtures[index], corporations[index]))
+      }
+    })
+
+    test('seeder function', async () => {
+      await seedDB();
+
+      //check roud
+      const round = await roundModel.find();
+      expect(round.length).toBe(1);
+      expect(compareWithDatabase(firstRound, round[0])).toBe(true);
+
+
+      //check tiles
+      const tiles = await tileModel.find();
+      for (const index in tiles) {
+        //get fixture that matches row and column
+        const fixtureTile = tileFixtures.find(tile => {
+          return tile.column === tiles[index].column && tile.row === tiles[index].row;
+        })
+        //check fixture is defined
+        expect(fixtureTile).toBeDefined();
+        //compare with db object
+        expect(compareWithDatabase(fixtureTile, tiles[index])).toBe(true);
+      }
+
+      //check corps
+      const corporations = await corporationModel.find();
+      for (const index in corporations) {
+        const fixtureCorporation = corporationFixtures.find(corporation => {
+          return corporation.name === corporations[index].name;
+        })
+        //check fixture is defined
+        expect(fixtureCorporation).toBeDefined();
+        //compare with db object
+        expect(compareWithDatabase(fixtureCorporation, corporations[index]))
       }
     })
   })
