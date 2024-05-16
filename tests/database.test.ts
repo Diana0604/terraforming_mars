afb236fbcd7a5681df2afce70fe5eb3cf2ac3e54
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2024-05-16 15:32:34 +0100

    add tests for getTiles function

--- tests/database.test.ts
+++ tests/database.test.ts
@@ -1,5 +1,5 @@
 import { createAllCorporations, createAllTiles, deleteAllModels, seedDB } from "../src/functions/database/database.seeder";
-import { closeDatabase, dbConnect } from "../src/functions/database/database.server";
+import { closeDatabase, dbConnect, getAllTiles } from "../src/functions/database/database.server";
 import tileFixtures from "../src/fixtures/tiles";
 import corporationFixtures from "../src/fixtures/corporation";
 import firstRound from "../src/fixtures/round";
@@ -7,6 +7,9 @@ import tileModel from "../src/functions/database/models/tile.model";
 import roundModel from "../src/functions/database/models/round.model";
 import corporationModel from "../src/functions/database/models/corporation.model";
 import { compareTiles, compareCorporations } from "../src/functions/comparers";
+import { COLONY_HUB } from "../src/showVariables";
+import buildingModel from "../src/functions/database/models/building.model";
+import { Building, Corporation } from "@/types";
 
 process.env.mongoUri = "mongodb://127.0.0.1:27017/terraforming-mars";
 
@@ -24,7 +27,7 @@ describe('database tests', () => {
 
     beforeEach(async () => {
       await deleteAllModels();
-    })
+    });
 
     test('tiles', async () => {
       const tiles = await createAllTiles();
@@ -54,6 +57,75 @@ describe('database tests', () => {
       corporations.sort(compareCorporations);
       expect(corporations).toMatchObject(corporationFixtures);
     })
+  })
+
+  describe('server', () => {
+
+    describe('get tiles', () => {
+      let corporation: Corporation | null;
+      let building: Building;
+      beforeAll(async () => {
+        //create tiles and corporations
+        await createAllTiles();
+        await createAllCorporations();
+
+        //get corporation and tile
+        corporation = await corporationModel.findOne();
+
+        if (!corporation) throw Error("test failed in prep phase");
+
+        const tile = await tileModel.findOne({ column: 'A', row: 1 });
+
+        //add building and colonization to tile
+        building = await buildingModel.create(COLONY_HUB);
+        tile.buildings.push(building._id);
+        tile.colonizedBy = corporation._id;
+        await tile.save();
+      })
+
+      test('not populating', async () => {
+
+        //get all tiles, info should be ids
+        const tiles = await getAllTiles(false);
+
+        //check necessary objects exist
+        if (!building._id) throw Error("there has been an error at the beforeAll step");
+        if (!corporation) throw Error("there has been an error at the beforeAll step");
+        if (!corporation._id) throw Error("there has been an error at the beforeAll step");
+
+        for (const tile of tiles) {
+
+          //if tile is A1 -> expect object id's to match
+          if (tile.column === 'A' && tile.row === 1) {
+            expect(tile.buildings[0]).toMatchObject(building._id);
+            expect(tile.colonizedBy).toMatchObject(corporation._id);
+            continue;
+          }
+
+          //else, expect buildings and colonized to be empty
+          expect(tile.buildings.length).toBe(0);
+          expect(tile.colonizedBy).toBeUndefined();
+        }
+      })
+
+      test('populating', async () => {
+        const tiles = await getAllTiles(true);
+        for (const tile of tiles) {
+          if (tile.column === 'A' && tile.row === 1) {
+            const tileBuilding = tile.buildings[0];
+
+            //stringify and parse are necessary to prevent Maximum Call Stack Size Exceeded (self reference)
+            expect(JSON.parse(JSON.stringify(tileBuilding))).toMatchObject(JSON.parse(JSON.stringify(building)));
+            continue;
+          }
+
+          //else, expect buildings and colonized to be empty
+          expect(tile.buildings.length).toBe(0);
+          expect(tile.colonizedBy).toBeUndefined();
+        }
+
+      })
+    });
   })
 
   afterAll(async () => {
