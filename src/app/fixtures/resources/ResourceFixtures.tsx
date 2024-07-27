ed2075caa64770f804a1e98ef973265e8e5ee3a0
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2024-07-27 19:15:15 +0200

    inputs for resource fixtures

--- src/app/fixtures/resources/ResourceFixtures.tsx
+++ src/app/fixtures/resources/ResourceFixtures.tsx
@@ -1,7 +1,42 @@
-import { Row, Col } from "antd"
+import { Row, Col } from "antd";
+import Input from "antd/es/input/Input";
 
 const ResourceFixtures = () => {
-  return <div>Resource Fixtures</div>
-}
+  return (
+    <>
+      <h4>List all resources that can be used / found</h4>
+      <Row>
+        <Col>
+          <Input defaultValue="Water"></Input>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Input defaultValue="Rare Metal"></Input>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Input defaultValue="Oxygen"></Input>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Input defaultValue="Food"></Input>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Input defaultValue="Synthetics"></Input>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Input defaultValue="Minerals"></Input>
+        </Col>
+      </Row>
+    </>
+  );
+};
 
-export default ResourceFixtures
+export default ResourceFixtures;
