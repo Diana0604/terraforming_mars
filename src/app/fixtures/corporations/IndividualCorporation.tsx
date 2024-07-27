dd42b9fe11f88f34f5a3e17e30469f5e9b247f6e
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2024-07-27 19:26:04 +0200

    corporation fixtures

--- src/app/fixtures/corporations/IndividualCorporation.tsx
+++ src/app/fixtures/corporations/IndividualCorporation.tsx
@@ -1,10 +1,59 @@
+import { Card, Col, Input, InputNumber, Row, Space } from "antd";
 
 interface IndividualCorporationProps {
-  name: string
+  name: string;
 }
 
-const IndividualCorporation = (props : IndividualCorporationProps) => {
-  return <div>Individual corporation: {props.name}</div>;
+const IndividualCorporation = (props: IndividualCorporationProps) => {
+  return (
+    <>
+      <Row>
+        <Col style={{ width: "140px" }}>Corporation Name</Col>
+        <Col>
+          <Input defaultValue={props.name}></Input>
+        </Col>
+      </Row>
+      <h4>Initial Resources</h4>
+      <Row>
+        <Col style={{ marginRight: "5px" }}>
+          <Space direction="vertical" size={16}></Space>
+          <Card size="small" title="Water">
+            <InputNumber />
+          </Card>
+        </Col>
+        <Col style={{ marginRight: "5px" }}>
+          <Space direction="vertical" size={16}></Space>
+          <Card size="small" title="Rare Metal">
+            <InputNumber />
+          </Card>
+        </Col>
+        <Col style={{ marginRight: "5px" }}>
+          <Space direction="vertical" size={16}></Space>
+          <Card size="small" title="Oxygen">
+            <InputNumber />
+          </Card>
+        </Col>
+        <Col style={{ marginRight: "5px" }}>
+          <Space direction="vertical" size={16}></Space>
+          <Card size="small" title="Food">
+            <InputNumber />
+          </Card>
+        </Col>
+        <Col style={{ marginRight: "5px" }}>
+          <Space direction="vertical" size={16}></Space>
+          <Card size="small" title="Synthetics">
+            <InputNumber />
+          </Card>
+        </Col>
+        <Col style={{ marginRight: "5px" }}>
+          <Space direction="vertical" size={16}></Space>
+          <Card size="small" title="Minerals">
+            <InputNumber />
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default IndividualCorporation;
