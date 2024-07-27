a9de55701d0d6b7694474b1ee68c83e52b241d31
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2024-07-27 18:22:46 +0200

    add fixtures page

---
+++ src/app/fixtures/page.tsx
@@ -0,0 +1,9 @@
+const Admin = () => {
+  return (
+    <div>This is the fixtures page. You can change the initial value of things here.
+      <div></div>
+    </div>
+  )
+}
+
+export default Admin
