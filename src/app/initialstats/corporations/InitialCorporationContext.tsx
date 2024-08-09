75545f119e19533c83d2724e436f66e0cebb8d7d
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2024-08-09 13:26:25 +0200

    update init corp context to use fetchGEt

--- src/app/initialstats/corporations/InitialCorporationContext.tsx
+++ src/app/initialstats/corporations/InitialCorporationContext.tsx
@@ -1,12 +1,8 @@
-"use client"
-import {
-  createContext,
-  ReactNode,
-  useEffect,
-  useState,
-} from "react";
+"use client";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import { Corporation } from "@/types";
 import { INIT_CORPORATION_ROUTE } from "@/constants";
+import { fetchGet } from "@/functions/database/database.fetchers";
 
 interface initialStatsProps {
   corporations: Corporation[];
@@ -21,18 +17,15 @@ export const InitialStatsContext = createContext<initialStatsProps>(initProps);
 const InitialStatsContextProvider = ({ children }: { children: ReactNode }) => {
   const [corporations, setCorporations] = useState<Corporation[]>([]);
 
-  useEffect(() => {
-    //get params for init corporations
-    fetch(INIT_CORPORATION_ROUTE, { method: "get" }).then(
-      async (response) => {
-        const data = await response.json();
-        setCorporations(data.corporations);
-      },
-      (error) => {
-        console.log("error on fetch", error);
-      }
-    );
-  }, []);
+  //fetch corporation callback
+  const fetchCorporationCallback = (data: { corporations: Corporation[] }) =>
+    setCorporations(data.corporations);
+
+  //get params for init corporations
+  useEffect(
+    () => fetchGet(INIT_CORPORATION_ROUTE, fetchCorporationCallback),
+    []
+  );
 
   return (
     <InitialStatsContext.Provider value={{ corporations }}>
