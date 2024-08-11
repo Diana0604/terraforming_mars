b406459a778595df24220ff2c443ed48778bde1b
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2024-08-11 09:28:41 +0100

    round stats - set hours / minutes / seconds

--- src/app/initialstats/round/RoundStats.tsx
+++ src/app/initialstats/round/RoundStats.tsx
@@ -1,19 +1,33 @@
 import { Col, InputNumber, Row } from "antd";
+import { useState } from "react";
 
 const RoundStats = () => {
+  const [hours, setHours] = useState<number>(0);
+  const [minutes, setMinutes] = useState<number>(0);
+  const [seconds, setSeconds] = useState<number>(0);
+
   return (
     <Row>
       <Col style={{ marginRight: "10px" }}>Hours: </Col>
       <Col style={{ marginRight: "5px" }}>
-        <InputNumber></InputNumber>
+        <InputNumber
+          value={hours}
+          onChange={(value) => setHours(Number(value))}
+        />
       </Col>
       <Col style={{ marginRight: "10px" }}>Minutes: </Col>
       <Col style={{ marginRight: "5px" }}>
-        <InputNumber></InputNumber>
+        <InputNumber
+          value={minutes}
+          onChange={(value) => setMinutes(Number(value))}
+        />
       </Col>
       <Col style={{ marginRight: "10px" }}>Seconds: </Col>
       <Col style={{ marginRight: "5px" }}>
-        <InputNumber></InputNumber>
+        <InputNumber
+          value={seconds}
+          onChange={(value) => setSeconds(Number(value))}
+        />
       </Col>
     </Row>
   );
