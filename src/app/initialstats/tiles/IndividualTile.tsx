a3129673bf63db88fed222ac8a7e7a84ecb344fe
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2024-08-17 17:52:20 +0100

    tile update complete

--- src/app/initialstats/tiles/IndividualTile.tsx
+++ src/app/initialstats/tiles/IndividualTile.tsx
@@ -1,53 +1,19 @@
-import { Col, Card, Checkbox, Row } from "antd";
-import { useContext } from "react";
-import { InitialResourcesContext } from "../resources/InitialResourcesContext";
-import { CheckboxChangeEvent } from "antd/es/checkbox";
-import Title from "antd/es/typography/Title";
+import { Col, Card } from "antd";
 import HazardsList from "./components/HazardsList";
 import UpdateStat from "../components/UpdateStat";
-import { IndividualTileContext } from "./IndividualTileContext";
 import LandmarkInput from "./components/LandmarkInput";
+import ResourcesList from "./components/ResourcesList";
+import { useContext } from "react";
+import { IndividualTileContext } from "./IndividualTileContext";
 
 const IndividualTile = () => {
-  const { resources } = useContext(InitialResourcesContext);
-  const { tile, updateResourcesAvailable, saveTile } = useContext(
-    IndividualTileContext
-  );
-
-  const onCheckboxChange = (event: CheckboxChangeEvent, value: string) => {
-    const newResources = JSON.parse(JSON.stringify(tile.resourcesAvailable));
-
-    // case checked -> add to resources
-    if (event.target.checked) {
-      newResources.push(value);
-      updateResourcesAvailable(newResources);
-    } else {
-      //case unchecked -> remove from resources
-      const index = newResources.indexOf(value);
-      newResources.splice(index, 1);
-
-      updateResourcesAvailable(newResources);
-    }
-  };
+  const {tile, saveTile} = useContext(IndividualTileContext);
 
   return (
     <Col className="mr-10 mb-10">
       <Card size={"default"} title={`${tile.column}${tile.row}`}>
         {/* resources */}
-        <Title level={5}>Resources</Title>
-
-        {resources.map((value, index) => {
-          return (
-            <Row key={index}>
-              <Checkbox
-                checked={tile.resourcesAvailable.includes(value)}
-                onChange={(event) => onCheckboxChange(event, value)}
-              >
-                {value}
-              </Checkbox>
-            </Row>
-          );
-        })}
+        <ResourcesList/>
 
         {/* landmark */}
         <LandmarkInput />
---
+++ src/app/initialstats/tiles/components/ResourcesList.tsx
@@ -0,0 +1,45 @@
+import { Checkbox, Row } from "antd";
+import { CheckboxChangeEvent } from "antd/es/checkbox";
+import Title from "antd/es/typography/Title";
+import { useContext } from "react";
+import {InitialResourcesContext} from "../../resources/InitialResourcesContext";
+import { IndividualTileContext } from "../IndividualTileContext";
+const ResourcesList = () => {
+
+  const { resources } = useContext(InitialResourcesContext);
+  const { tile, updateResourcesAvailable } = useContext(IndividualTileContext);
+
+  const onCheckboxChange = (event: CheckboxChangeEvent, value: string) => {
+    const newResources = JSON.parse(JSON.stringify(tile.resourcesAvailable));
+
+    // case checked -> add to resources
+    if (event.target.checked) {
+      newResources.push(value);
+      updateResourcesAvailable(newResources);
+    } else {
+      //case unchecked -> remove from resources
+      const index = newResources.indexOf(value);
+      newResources.splice(index, 1);
+
+      updateResourcesAvailable(newResources);
+    }
+  };
+  
+  return (<><Title level={5}>Resources</Title>
+
+    {resources.map((value, index) => {
+      return (
+        <Row key={index}>
+          <Checkbox
+            checked={tile.resourcesAvailable.includes(value)}
+            onChange={(event) => onCheckboxChange(event, value)}
+          >
+            {value}
+          </Checkbox>
+        </Row>
+      );
+    })}</>)
+
+}
+
+export default ResourcesList;
