6e3e6e307459cf7372ae5657e6da09c1a70f4504
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2024-08-17 21:54:00 +0100

    beautify building menu

--- src/app/actors/corporation/IndividualCorporation/build/BuildingStats.tsx
+++ src/app/actors/corporation/IndividualCorporation/build/BuildingStats.tsx
@@ -1,78 +1,85 @@
-"use client"
+"use client";
 //constants
-import { PLAYER1_CORPORATION_NAME, PLAYER2_CORPORATION_NAME } from "@/fixtures/corporations.fixtures"
+import {
+  PLAYER1_CORPORATION_NAME,
+  PLAYER2_CORPORATION_NAME,
+} from "@/fixtures/corporations.fixtures";
 
 //types
-import { Corporation, Tile } from "@/types"
-import { useContext } from "react"
+import { Corporation, Tile } from "@/types";
+import { useContext } from "react";
 
 //contexts
-import { CorporationsContext } from "@/contexts/CorporationsContexts"
+import { IndividualCorporationContext } from "../IndividualCorporationContext";
 
 //components
-import Build from "./Build"
-import { Button } from "antd"
-import { BUILD_DATABASE_ROUTE } from "@/constants"
-
-interface IBuildingStatsProps {
-
-  corporationName: string,
-}
+import Build from "./Build";
+import { Button, Card, Row } from "antd";
+import { BUILD_DATABASE_ROUTE } from "@/constants";
 
 /**
  * Display stats of all buildings owned by a specific corporation
  */
-const BuildingStats = (props: IBuildingStatsProps) => {
-
-  const corporations = useContext(CorporationsContext)
+const BuildingStats = () => {
+  const corporation = useContext(IndividualCorporationContext);
 
   //setup the display for each corporation
-  const setupCorportaion = (corporation: Corporation) => {
-    return <div>
-      <div>
+
+  return (
+    <Card>
+      {/* currently owned */}
+      <Row>
         Buildings currently owned by this corporation:
-        {
-          corporation.buildingsOwned.map((building, index) => {
-            building.tile = building.tile as Tile
-            return (
-              <div key={index}>
-                <div style={{ fontWeight: "bold" }}>Tile: {`${building.tile.column}`}{building.tile.row}</div>
-                <span>Type: {building.buildingType}</span>
-                <Button onClick={async () =>{
-                  const res = await fetch(BUILD_DATABASE_ROUTE, {method: 'delete', body: JSON.stringify({id: building._id})})
-                }}>Destroy building</Button>
+        {corporation.buildingsOwned.map((building, index) => {
+          building.tile = building.tile as Tile;
+          return (
+            <div key={index}>
+              <div style={{ fontWeight: "bold" }}>
+                Tile: {`${building.tile.column}`}
+                {building.tile.row}
               </div>
-            )
-          })
-        }
-      </div>
-      <div>
+              <span>Type: {building.buildingType}</span>
+              <Button
+                onClick={async () => {
+                  const res = await fetch(BUILD_DATABASE_ROUTE, {
+                    method: "delete",
+                    body: JSON.stringify({ id: building._id }),
+                  });
+                }}
+              >
+                Destroy building
+              </Button>
+            </div>
+          );
+        })}
+      </Row>
+
+      {/* next round */}
+      <Row>
         Buildings that will be built in the next round:
-        {
-          corporation.newBuildingsNextRound?.map((building, index) => {
-            building.tile = building.tile as Tile
-            return (
-              <div key={index}>
-                <div style={{ fontWeight: "bold" }}>Type: {building.buildingType}</div>
-                <div>Tile: {`${building.tile.column}`}{building.tile.row}</div>
+        {corporation.newBuildingsNextRound?.map((building, index) => {
+          building.tile = building.tile as Tile;
+          return (
+            <div key={index}>
+              <div style={{ fontWeight: "bold" }}>
+                Type: {building.buildingType}
               </div>
-            )
-          })
-        }
-      </div>
-      < Build corporationName={corporation.name} corporationId={corporation._id}></Build>
-    </div >
-  }
+              <div>
+                Tile: {`${building.tile.column}`}
+                {building.tile.row}
+              </div>
+            </div>
+          );
+        })}
+      </Row>
 
-  //call setupCorporation depending on corporation name
-  if (props.corporationName === PLAYER1_CORPORATION_NAME) {
-    return setupCorportaion(corporations[0])
-  }
-  if (props.corporationName === PLAYER2_CORPORATION_NAME) {
-    return setupCorportaion(corporations[1])
-  }
+      {/* build new */}
+      <Build
+        corporationName={corporation.name}
+        corporationId={corporation._id}
+      ></Build>
+    </Card>
+  );
+};
 
-  return <>Unknown Corporation</>
-}
-
-export default BuildingStats
+export default BuildingStats;
