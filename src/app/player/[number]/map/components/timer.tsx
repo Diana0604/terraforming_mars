7fd48beb870f9fc143422bee3da294ba71e7caa9
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2024-08-18 11:27:06 +0100

    set timer to also work with seconds per round

--- src/app/player/[number]/map/components/timer.tsx
+++ src/app/player/[number]/map/components/timer.tsx
@@ -1,64 +1,72 @@
-"use client"
-import { useContext, useEffect, useState } from 'react'
-import styles from '../map.module.css'
-import { Round } from '@/types';
-import { RoundContext } from '@/contexts/RoundContext';
-import { SECONDS_PER_ROUND } from '@/fixtures/round.fixtures';
-
+"use client";
+import { useContext, useEffect, useState } from "react";
+import styles from "../map.module.css";
+import { Round } from "@/types";
+import { RoundContext } from "@/contexts/RoundContext";
+import { fetchGet } from "@/functions/database/database.fetchers";
+import { INITSTATS_ROUTE } from "@/constants";
 
 const Timer = () => {
+  const [hydrated, setHydrated] = useState(false);
+  //time in milliseconds
+  const [displayTime, setDisplayTime] = useState<Date>(new Date(Date.now()));
+  const [round, setRound] = useState<Round>();
+  const roundContext = useContext(RoundContext);
+  const [secondsPerRound, setSecondsPerRound] = useState<number>(0);
 
-    const [hydrated, setHydrated] = useState(false);
-    //time in milliseconds
-    const [displayTime, setDisplayTime] = useState<Date>(new Date(Date.now()))
-    const [round, setRound] = useState<Round>();
-    const roundContext = useContext(RoundContext);
+  useEffect(() => {
+    const resetClock = () => {
+      setDisplayTime((time) => {
+        if (roundContext.round.startTime) {
+          let newTime = new Date();
+          newTime.setHours(0, 0, 0, 0);
+          let startTime: Date = roundContext.round.startTime;
+          let elapsedTime = Date.now() - startTime.getTime();
 
-    useEffect(() => {
+          let elapsedGameTime = elapsedTime * (86400 / secondsPerRound);
+          time.setTime(newTime.getTime() + elapsedGameTime);
 
-        const resetClock = () => {
+          //floor to nearest 5 minutes
+          time.setMinutes(time.getMinutes() - (time.getMinutes() % 5));
+        }
+        return time;
+      });
+    };
 
-          setDisplayTime(time => {
-            if (roundContext.round.startTime) {
-            let newTime = new Date()
-            newTime.setHours(0,0,0,0)
-            let startTime:Date = roundContext.round.startTime
-            let elapsedTime = (Date.now() - startTime.getTime())
+    setHydrated(true);
+    //initial display of time
+    if (roundContext.round.playing && !roundContext.round.darkHour)
+      resetClock();
+  }, [roundContext.round, secondsPerRound]);
 
-            let elapsedGameTime = elapsedTime*(86400/SECONDS_PER_ROUND)
-            time.setTime(newTime.getTime() + elapsedGameTime);
+  useEffect(() => {
+    setRound(roundContext.round);
+  }, [roundContext.round]);
 
-            //floor to nearest 5 minutes
-            time.setMinutes(time.getMinutes() - time.getMinutes()%5)
+  const getInitStatsCallback = (data: any) =>
+    setSecondsPerRound(data.secondsPerRound);
 
-            }
-            return time
-          })
+  useEffect(() => {
+    fetchGet(INITSTATS_ROUTE, getInitStatsCallback);
+  }, []);
 
-        
-      }
+  if (!hydrated) {
+    // Returns null on first render, so the client and server match
+    return null;
+  }
 
-      setHydrated(true);
-      //initial display of time
-      if(roundContext.round.playing && !roundContext.round.darkHour) resetClock()
-    }, [roundContext.round])
-
-    useEffect(() => {
-      setRound(roundContext.round)
-    }, [roundContext.round])
-
-    if (!hydrated) {
-      // Returns null on first render, so the client and server match
-      return null;
-    }
-
-    return (
-      <div>
-        <div className={styles.timer} >
-          { round?.darkHour ? "Dark Hour" : displayTime.toLocaleTimeString('en-GB', {hour: '2-digit', minute:'2-digit'}) }
-        </div>
+  return (
+    <div>
+      <div className={styles.timer}>
+        {round?.darkHour
+          ? "Dark Hour"
+          : displayTime.toLocaleTimeString("en-GB", {
+              hour: "2-digit",
+              minute: "2-digit",
+            })}
       </div>
-    )
-}
+    </div>
+  );
+};
 
 export default Timer;
