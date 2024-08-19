235ed8e1a6cd1f2d8161cde04c99f97d0627f4f3
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2024-08-19 17:19:19 +0100

    recover first find upon server created

--- src/functions/database/database.server.ts
+++ src/functions/database/database.server.ts
@@ -1,5 +1,14 @@
 import mongoose from "mongoose";
 import tileModel from "./models/tile.model";
+import buildingModel from "./models/building.model";
+import corporationModel from "./models/corporation.model";
+import roundModel from "./models/round.model";
+import initialbuildingModel from "./models/initialstats/initialbuilding.model";
+import initialcorpsModel from "./models/initialstats/initialcorps.model";
+import initialresourcesModel from "./models/initialstats/initialresources.model";
+import initialstatsModel from "./models/initialstats/initialstats.model";
+import alertModel from "./models/alert.model";
+import resourceModel from "./models/resource.model";
 
 //================== TO BE CALLED FROM SERVER SIDE ==================
 export const dbConnect = async () => {
@@ -17,6 +26,25 @@ export const dbConnect = async () => {
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("================== MONGOOSE CONNECTED ==================");
+
+    // a first find is needed upon first connection due to a mongoose issue with model definitions
+    // game models
+    await alertModel.find();
+    await buildingModel.find();
+    await corporationModel.find();
+    await resourceModel.find();
+    await roundModel.find();
+    await tileModel.find();
+    
+    // initial stats models
+    await initialbuildingModel.find();
+    await initialcorpsModel.find();
+    await initialresourcesModel.find();
+    await initialstatsModel.find();
+
+    console.log(
+      "=================== MODELS DEFINED =========================="
+    );
   } catch (error) {
     throw error;
   }
