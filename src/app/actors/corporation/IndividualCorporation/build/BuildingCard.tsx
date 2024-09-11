7a75375ce39aa2e1bd9687bcde039350cd87790b
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2024-09-11 15:50:29 +0200

    use fetchDelete in actors -> BuildingCard

--- src/app/actors/corporation/IndividualCorporation/build/BuildingCard.tsx
+++ src/app/actors/corporation/IndividualCorporation/build/BuildingCard.tsx
@@ -1,4 +1,5 @@
 import { BUILD_DATABASE_ROUTE } from "@/constants";
+import { fetchDelete } from "@/functions/database/database.fetchers";
 import { Building } from "@/types";
 import { DeleteFilled } from "@ant-design/icons";
 import { Card } from "antd";
@@ -8,12 +9,7 @@ const BuildingCard = (building: Building) => {
     <Card style={{ width: 150 }}>
       <span>{building.buildingType}</span>
       <DeleteFilled
-        onClick={async () => {
-          const res = await fetch(BUILD_DATABASE_ROUTE, {
-            method: "delete",
-            body: JSON.stringify({ id: building._id }),
-          });
-        }}
+        onClick={() => fetchDelete(BUILD_DATABASE_ROUTE, { id: building._id })}
         className="delete-icon align-righ"
       />
     </Card>
