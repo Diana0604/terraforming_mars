ee3d2b3e69c7ff41f6e379d305f06c8a7fff8301
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2024-09-11 16:16:06 +0200

    update round context to use fetchGet + useIterval

--- src/contexts/RoundContext.tsx
+++ src/contexts/RoundContext.tsx
@@ -1,63 +1,47 @@
-
 import { ROUND_MANAGER_ROUTE } from "@/constants";
+import { fetchGet } from "@/functions/database/database.fetchers";
 import { Round } from "@/types";
 import React, { useState, useEffect } from "react";
+import { useInterval } from "usehooks-ts";
 
 //tiles can be accessed from context
 type contextProps = {
-  round: Round
-}
+  round: Round;
+};
 
 //initial corporation values
 const initialRoundProps: contextProps = {
   round: {
     number: 0,
     playing: false,
-    darkHour: false
-  }
+    darkHour: false,
+  },
 };
 
 //build context
-export const RoundContext: React.Context<contextProps> = React.createContext(initialRoundProps);
+export const RoundContext: React.Context<contextProps> =
+  React.createContext(initialRoundProps);
 
 //build context provider
-export const RoundContextProvider = ({ children }: { children: React.ReactNode }) => {
-
+export const RoundContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
   //corporation objects and setters
-  const [round, setRound] = useState<Round>(initialRoundProps.round)
+  const [round, setRound] = useState<Round>(initialRoundProps.round);
 
+  const updateRoundCallback = (data: Round) => {
+    if (data && data.startTime) data.startTime = new Date(data.startTime);
+    setRound(data);
+  };
 
-  useEffect(() => {
+  const updateRound = () => fetchGet(ROUND_MANAGER_ROUTE, updateRoundCallback);
 
-    //update round info every second by fetching from database
-    const updateRound = () => {
-      try {
-        fetch(ROUND_MANAGER_ROUTE, { method: 'get' }).then(async response => {
-          const data = await response.json();
-          if(data && data.startTime)
-            data.startTime = new Date(data.startTime)
-          setRound(data)
-        }, (error) => { console.log('error on fetch', error) })
-      } catch (error) {
-        console.log('error on fetch', error)
-      }
-    }
-
-    updateRound()
-
-    const interval = setInterval(updateRound, 1000)
-
-    //destroy interval at dismount
-    return () => {
-      clearInterval(interval)
-    }
-  }, [])
+  useInterval(updateRound, 1000);
 
   //return provider values
   return (
-    <RoundContext.Provider value={{ round }}>
-      {children}
-    </RoundContext.Provider>
+    <RoundContext.Provider value={{ round }}>{children}</RoundContext.Provider>
   );
 };
-
