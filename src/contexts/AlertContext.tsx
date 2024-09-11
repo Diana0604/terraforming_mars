6151c69253ecfd0a60a4304191d1ada1747c11ad
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2024-09-11 16:12:15 +0200

    alert message

--- src/contexts/AlertContext.tsx
+++ src/contexts/AlertContext.tsx
@@ -1,52 +1,41 @@
-
 import { ALERT_MANAGER_ROUTE } from "@/constants";
+import { fetchGet } from "@/functions/database/database.fetchers";
 import React, { useState, useEffect } from "react";
+import { useInterval } from "usehooks-ts";
 
 //tiles can be accessed from context
 type contextProps = {
-    message: string;
-}
+  message: string;
+};
 
 //initial corporation values
 const initialAlertProps: contextProps = {
-    message: ""
+  message: "",
 };
 
 //build context
-export const AlertContext: React.Context<contextProps> = React.createContext(initialAlertProps);
+export const AlertContext: React.Context<contextProps> =
+  React.createContext(initialAlertProps);
 
 //build context provider
-export const AlertContextProvider = ({ children }: { children: React.ReactNode }) => {
-
+export const AlertContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
   //corporation objects and setters
-  const [message, setMessage] = useState<string>(initialAlertProps.message)
+  const [message, setMessage] = useState<string>(initialAlertProps.message);
 
+  const updateMessageCallback = (data: { message: string }) => {
+    if (data) setMessage(data.message);
+    else setMessage("");
+  };
 
-  useEffect(() => {
+  //update round info every second by fetching from database
+  const updateMessage = () =>
+    fetchGet(ALERT_MANAGER_ROUTE, updateMessageCallback);
 
-    //update round info every second by fetching from database
-    const updateMessage = () => {
-      try {
-        fetch(ALERT_MANAGER_ROUTE, { method: 'get' }).then(async response => {
-          const data = await response.json();
-          if(data)
-            setMessage(data.message)
-          else setMessage("");
-        }, (error) => { console.log('error on fetch', error) })
-      } catch (error) {
-        console.log('error on fetch', error)
-      }
-    }
-
-    updateMessage()
-
-    const interval = setInterval(updateMessage, 1000)
-
-    //destroy interval at dismount
-    return () => {
-      clearInterval(interval)
-    }
-  }, [])
+  useInterval(updateMessage, 1000);
 
   //return provider values
   return (
@@ -55,4 +44,3 @@ export const AlertContextProvider = ({ children }: { children: React.ReactNode }
     </AlertContext.Provider>
   );
 };
-
